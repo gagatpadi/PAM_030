eedf4a0c8a2b8deadbf325370b40f8a6f28f8dd9
ASUS <padiyogaradhana@gmail.com>
2026-01-17 16:05:11 +0700

    MyKonterApp.kt

---
+++ app/src/main/java/com/example/mykonter/MyKonterApp.kt
@@ -0,0 +1,12 @@
+package com.example.mykonter
+
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.runtime.Composable
+import com.example.mykonter.navigation.AppNav
+
+@Composable
+fun MyKonterApp() {
+    MaterialTheme {
+        AppNav()
+    }
+}
