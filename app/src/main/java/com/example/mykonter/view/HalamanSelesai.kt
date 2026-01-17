d362b19d46e8e504f1e0ad79dcf5cd3de8a0bbcf
ASUS <padiyogaradhana@gmail.com>
2026-01-17 16:04:52 +0700

    HalamanSelesai.kt

---
+++ app/src/main/java/com/example/mykonter/view/HalamanSelesai.kt
@@ -0,0 +1,11 @@
+package com.example.mykonter.view
+
+
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.navigation.NavController
+
+@Composable
+fun HalamanSelesai(nav: NavController) {
+    Text("Servis selesai")
+}
