a7ce72886a8bf36a79aff684bd06029e190c29b5
ASUS <padiyogaradhana@gmail.com>
2026-01-17 16:04:11 +0700

    gradle

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -51,8 +51,12 @@ dependencies {
     implementation(libs.androidx.compose.ui.tooling.preview)
     implementation(libs.androidx.compose.material3)
 
+    implementation("androidx.navigation:navigation-compose:2.7.7")
+
     implementation("com.squareup.retrofit2:retrofit:2.9.0")
     implementation("com.squareup.retrofit2:converter-gson:2.9.0")
+    implementation("androidx.compose.material:material-icons-extended")
+
 
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
